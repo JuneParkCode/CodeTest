d86ce53bb908ac2fc2777ae8add5e6a866468629
June <khsj15@khu.ac.kr>
2021-09-05 23:59:36 +0900

    comment : 메뉴 리뉴얼

--- Level_2/메뉴 리뉴얼/renew.js
+++ Level_2/메뉴 리뉴얼/renew.js
@@ -1,17 +1,22 @@
 function solution(orders, course) {
 	const resMap = new Map();
+	// 메뉴 순서 정렬
 	const refine = orders.map((order) => {
 		return order.split("").sort().join("");
 	});
+	// Map을 통해서 조합 빈도 계산
 	refine.forEach((order) => {
 		course.forEach((n) => {
 			combination(resMap, order, "", n, 0, 0, n);
 		});
 	});
+	// 계산을 용이하게 하기 위해 Map -> Array
 	const arr = Array.from(resMap);
+	// 계산을 용이하게 하기 위해 코스 개수별로 저장
 	const a = course.map((n) => {
 		return arr.filter((elem) => elem[0].length === n);
 	});
+	// 각 코스 개수마다 최다 빈도를 알아냄
 	const max = a
 		.map((elem) => {
 			return elem.reduce((res, cur) => {
@@ -19,6 +24,7 @@ function solution(orders, course) {
 			}, 0);
 		})
 		.map((n) => (n > 1 ? n : 0));
+	// 최다 빈도에 해당되는 요소들을 추려내고 결과물에 맞게 정제함
 	const result = a
 		.map((elem, idx) => {
 			return elem
